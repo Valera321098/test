f1d0080aae779ebdb5ba439bab879279d673e88d
Valera <valera321098@gmail.com>
2021-03-13 22:57:01 +0200

    commit

---
+++ src/test/java/OneTest.java
@@ -0,0 +1,18 @@
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+public class OneTest {
+
+    @Test
+    public void test() {
+        assertEquals(2, 2);
+        System.out.println("------------TEST-------------");
+    }
+
+    @Test
+    public void test2() {
+        assertEquals(2, 2);
+        System.out.println("------------TEST 2-------------");
+    }
+}
